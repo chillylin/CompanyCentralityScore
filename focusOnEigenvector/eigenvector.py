e198e20ef01e5adeadb382fc8e728250074b16d8
Chillylin the Beaver <32764773+chillylin@users.noreply.github.com>
2018-11-26 09:52:17 +1300

    Update eigenvector.py

--- focusOnEigenvector/eigenvector.py
+++ focusOnEigenvector/eigenvector.py
@@ -2,19 +2,6 @@ import pandas as pd
 from graph_tool.all import *
 from multiprocessing import Pool
 
-
-def decider(row, currentCompany, currentCountry):
-    if (row[3] == currentCountry and row[4] == currentCountry) and (row[0]!=currentCompany or row[6] == 'C'):
-
-        # First part: countries which are NOT the current country are kept, jump to else directly
-        # Second parenses: (both countries are current country)
-            # if the company side is not the company, drop (ignoring the other side)
-            # if the company side is the company but the other side is a company drop, too.
-        return False
-    else:
-        return True
-
-
 # get a df and return the graph with a list of names of nodes
 def calc(tup):
 
@@ -22,8 +9,11 @@ def calc(tup):
     company = tup[1]
     country = tup[2]
 
-    df = inputdf[inputdf.apply (lambda row: decider(row, company, country),axis=1)].copy()
-
+    df = inputdf[~(((inputdf.iloc[:,3] == country) & (inputdf.iloc[:,4] == country)) & ((inputdf.iloc[:,0] != company ) | (inputdf.iloc[:,6] =='C')))]
+    # First part: countries which are NOT the current country are kept, jump to else directly
+    # Second parenses: (both countries are current country)
+            # if the company side is not the company, drop (ignoring the other side)
+            # if the company side is the company but the other side is a company drop, too.
     # first [0] and second [1] columns are nodes, the third [2] column is weight
 
     # declare the graph
